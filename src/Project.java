1740efd340535beb84974aa6e73513c14a624ba7
Marouane <mtayab1994@gmail.com>
2017-10-22 16:23:06 +0200

    isWellSortedWithEmptyStackandQueue

--- src/Project.java
+++ src/Project.java
@@ -9,6 +9,8 @@ public class Project implements ProjectInterface {
 	
 	public boolean isWellSorted(String[] strings){
 		boolean isSorted = false;
+		if(strings.length ==0 && s.length == 0)
+			return true;
 		for(int i=0; i<strings.length-1; i++ )
 		{
 			isSorted = false;
