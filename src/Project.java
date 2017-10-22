c8b85cf1b208d8fda00f589abc505f902cafff71
Johannes Heimbach <heimbachjohannes@gmail.com>
2017-10-22 17:10:57 +0200

    simplify if statements

--- src/Project.java
+++ src/Project.java
@@ -1,12 +1,12 @@
 
 public class Project implements ProjectInterface {
 	public String[][] s;
-	
+
 	public Project(String[][] strings)
 	{
 		s = strings;
 	}
-	
+
 	public boolean isWellSorted(String[] strings){
 		boolean isSorted = false;
 		if(strings.length ==0 && s.length == 0)
@@ -16,23 +16,20 @@ public class Project implements ProjectInterface {
 			isSorted = false;
 			for(int j=i+1; j<strings.length; j++)
 			{
-				
+
 				for(int k=0; k< s.length; k++)
 				{
-					
+
 					if((strings[i].equals(s[k][0]) && strings[j].equals(s[k][1])))
 						{
 							isSorted = true;
 						}
 				}
-				
+
 			}
-			if(isSorted == false)
+			if(!isSorted)
 				break;
 		}
-		if(isSorted)
-			return true;
-		else
-			return false;
+		return isSorted;
 	}
 }
